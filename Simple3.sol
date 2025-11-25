7248b6e45de75cc5855e237b20695b97ea57a4da
Velo2Compet <velo2compet@gmail.com>
2025-11-25 18:36:15 +0100

    Update Simple3.sol

--- Simple3.sol
+++ Simple3.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.17;
 
 contract SimpleInteraction {
-    uint256 public totalInteractions; //blblbl
+    uint256 public totalInteractions; 
 
     mapping(address => uint256[]) private _userCounts;
 
