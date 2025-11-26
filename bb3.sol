4f051ccadc17f589693eba31c9612361d4e0838e
Velo2Compet <velo2compet@gmail.com>
2025-11-26 15:09:50 +0100

    Update bb3.sol

--- bb3.sol
+++ bb3.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.22;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract V2CERC20 is ERC20, Ownable {
+contract V2CERC20 is ERC20, Ownable { //ddfg
     uint256 public immutable maxSupply;
 
     constructor(
